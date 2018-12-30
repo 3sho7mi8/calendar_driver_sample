2319d7c824652a38e9455f7768956a3d9578d5db
3sho7mi8 <hiro.pinot@gmail.com>
2018-12-30 13:37:59 +0900

    Add OAuth2.0 webserver application

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Calendar Driver Sample desu...
+
+## USAGE
+
+```
+$ ruby oauth_webserver_application.rb
+```
--- oauth_webserver_application.rb
+++ oauth_webserver_application.rb
@@ -1,18 +1,26 @@
 require 'bundler/setup'
 require 'pry'
-require 'dotenv'
+require 'dotenv/load'
 require 'google/apis/drive_v2'
 require 'google/api_client/client_secrets'
 
-binding.pry
+# reference: https://developers.google.com/api-client-library/ruby/auth/web-app?authuser=2
 client_secrets = Google::APIClient::ClientSecrets.load
 auth_client = client_secrets.to_authorization
 auth_client.update!(
   :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
-  :redirect_uri => 'http://www.example.com/oauth2callback',
+  :redirect_uri => ENV['REDIRECT_URI'],
   :additional_parameters => {
     "access_type" => "offline",         # offline access
-    "include_granted_scopes" => "true"  # incremental auth
+    "include_granted_scopes" => "true", # incremental auth
+    "state" => "company_name"
   }
 )
 
+auth_uri = auth_client.authorization_uri.to_s
+puts '出力されたURLにアクセスしてください'
+puts auth_uri
+
+puts 'CODEを入力してください'
+auth_client.code = gets
+auth_client.fetch_access_token!
